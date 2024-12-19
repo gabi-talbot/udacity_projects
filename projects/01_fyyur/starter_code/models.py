d46e3dfac2ae768c307fbcde11fa667710eeb901
Gabi <talbotgabi@gmail.com>
2024-12-19 14:44:17 +0000

    work on venue submit and retriev by id

--- projects/01_fyyur/starter_code/models.py
+++ projects/01_fyyur/starter_code/models.py
@@ -2,16 +2,15 @@
 # Imports
 #----------------------------------------------------------------------------#
 from flask_sqlalchemy import SQLAlchemy
-
 from sqlalchemy import PrimaryKeyConstraint
 
 db=SQLAlchemy()
 
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venue'
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     city = db.Column(db.String(120), nullable=False)
     state = db.Column(db.String(120), nullable=False)
@@ -21,14 +20,16 @@ class Venue(db.Model):
     genres = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
-    artists = db.relationship('Artist', secondary='Show', backref=db.backref('shows', lazy='True'))
+    artists = db.relationship('Artist', secondary='show', backref=db.backref('shows'))
 
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name} {type(self.genres)}>'
     # COMPLETED implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
@@ -42,14 +43,20 @@ class Artist(db.Model):
     seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
     seeking_description = db.Column(db.String(500))
 
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
+
     # COMPLETED: implement any missing fields, as a database migration using Flask-Migrate
 class Show(db.Model):
-    __tablename__ = 'Show'
+    __tablename__ = 'show'
     __table_args__ = (
       PrimaryKeyConstraint('venue_id', 'artist_id'),
     )
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
-    artist_id = db.Column( db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), primary_key=True)
+    artist_id = db.Column( db.Integer, db.ForeignKey('artist.id'), primary_key=True)
     start_time = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<Show {self.venue_id} {self.artist_id} {self.start_time}>'
 
 # COMPLETED Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
